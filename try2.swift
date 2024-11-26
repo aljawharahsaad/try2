359bac0306d952bb096c10f043fb38347190a58d
aljawharah <aljawharah@qawam.ai>
2024-11-26 13:28:34 +0300

    try

---
+++ try2.swift
@@ -0,0 +1,22 @@
+//
+//  numbers.swift
+//  try2
+//
+//  Created by aljawharah almasoud on 24/05/1446 AH.
+//
+
+import Foundation
+
+public struct try2 {
+    public static func sumOfNums() -> Int {
+        let num1 = 5
+        let num2 = secNum()
+        return num1 + num2
+    }
+    static func secNum() -> Int {
+        let num1 = 5
+        let num2 = 2
+        let sum = num1 + num2
+        return sum
+    }
+}
