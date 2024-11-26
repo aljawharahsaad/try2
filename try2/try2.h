ddc47eb43c6615dad018c3f6898a6973441ad5a3
aljawharah <aljawharah@qawam.ai>
2024-11-26 13:08:39 +0300

    Initial Commit

---
+++ try2/try2.h
@@ -0,0 +1,18 @@
+//
+//  try2.h
+//  try2
+//
+//  Created by aljawharah almasoud on 24/05/1446 AH.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for try2.
+FOUNDATION_EXPORT double try2VersionNumber;
+
+//! Project version string for try2.
+FOUNDATION_EXPORT const unsigned char try2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <try2/PublicHeader.h>
+
+
